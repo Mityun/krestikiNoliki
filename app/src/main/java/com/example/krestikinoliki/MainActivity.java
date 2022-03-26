afd990eede14d5a3d47e174f38d1f66762967654
Mitya <you@example.com>
2022-03-26 20:56:47 +0300

    возможность играть вдвоем(без проверок победы)

--- app/src/main/java/com/example/krestikinoliki/MainActivity.java
+++ app/src/main/java/com/example/krestikinoliki/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     int r1, r2;
     String var1 = "X";
     String var2 = "O";
+    boolean state = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,61 +33,84 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
     }
     /// human move
-
     public void but1(View v){
         board[0][0] = var1;
-        button1.setText(var1);
+        if (state) {
+            button1.setText(var1);
+        } else button1.setText(var2);
+        state = !state;
         button1.setEnabled(false);
     }
 
     public void but2(View v){
         board[0][1] = var1;
-        button2.setText(var1);
+        if (state) {
+            button2.setText(var1);
+        } else button2.setText(var2);
+        state = !state;
         button2.setEnabled(false);
     }
 
     public void but3(View v){
         board[0][2] = var1;
-        button3.setText(var1);
+        if (state) {
+            button3.setText(var1);
+        } else button3.setText(var2);
+        state = !state;
         button3.setEnabled(false);
     }
 
     public void but4(View v){
         board[1][0] = var1;
-        button4.setText(var1);
+        if (state) {
+            button4.setText(var1);
+        } else button4.setText(var2);
+        state = !state;
         button4.setEnabled(false);
     }
 
     public void but5(View v){
         board[1][1] = var1;
-        button5.setText(var1);
+        if (state) {
+            button5.setText(var1);
+        } else button5.setText(var2);
+        state = !state;
         button5.setEnabled(false);
     }
 
     public void but6(View v){
         board[1][2] = var1;
-        button6.setText(var1);
+        if (state) {
+            button6.setText(var1);
+        } else button6.setText(var2);
+        state = !state;
         button6.setEnabled(false);
     }
 
     public void but7(View v){
         board[2][0] = var1;
-        button7.setText(var1);
+        if (state) {
+            button7.setText(var1);
+        } else button7.setText(var2);
+        state = !state;
         button7.setEnabled(false);
     }
 
     public void but8(View v){
         board[2][1] = var1;
-        button8.setText(var1);
+        if (state) {
+            button8.setText(var1);
+        } else button8.setText(var2);
+        state = !state;
         button8.setEnabled(false);
     }
 
     public void but9(View v){
         board[2][2] = var1;
-        button9.setText(var1);
+        if (state) {
+            button9.setText(var1);
+        } else button9.setText(var2);
+        state = !state;
         button9.setEnabled(false);
     }
-
-
-    /// bot move
 }
